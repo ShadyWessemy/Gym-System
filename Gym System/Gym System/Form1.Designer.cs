d40fa633a5aa582e5ebd65caf31b29737cc934bb
Shady Mohamed Elwessemy <shady.mohamed969@gmail.com>
2021-12-27 21:22:57 +0200

    Add files via upload

---
+++ Gym System/Gym System/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Gym_System
+{
+    partial class loginform
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginform));
+            this.usernamelabel = new System.Windows.Forms.Label();
+            this.passwordlabel = new System.Windows.Forms.Label();
+            this.usernametextbox = new System.Windows.Forms.TextBox();
+            this.passwordtextbox = new System.Windows.Forms.TextBox();
+            this.loginbutton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.loginpicture = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.loginpicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usernamelabel
+            // 
+            this.usernamelabel.AutoSize = true;
+            this.usernamelabel.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.usernamelabel.Location = new System.Drawing.Point(277, 237);
+            this.usernamelabel.Name = "usernamelabel";
+            this.usernamelabel.Size = new System.Drawing.Size(121, 31);
+            this.usernamelabel.TabIndex = 0;
+            this.usernamelabel.Text = "Username";
+            // 
+            // passwordlabel
+            // 
+            this.passwordlabel.AutoSize = true;
+            this.passwordlabel.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.passwordlabel.Location = new System.Drawing.Point(277, 302);
+            this.passwordlabel.Name = "passwordlabel";
+            this.passwordlabel.Size = new System.Drawing.Size(114, 31);
+            this.passwordlabel.TabIndex = 1;
+            this.passwordlabel.Text = "Password";
+            // 
+            // usernametextbox
+            // 
+            this.usernametextbox.Location = new System.Drawing.Point(441, 243);
+            this.usernametextbox.Name = "usernametextbox";
+            this.usernametextbox.Size = new System.Drawing.Size(125, 27);
+            this.usernametextbox.TabIndex = 2;
+            // 
+            // passwordtextbox
+            // 
+            this.passwordtextbox.Location = new System.Drawing.Point(441, 302);
+            this.passwordtextbox.Name = "passwordtextbox";
+            this.passwordtextbox.PasswordChar = '*';
+            this.passwordtextbox.Size = new System.Drawing.Size(125, 27);
+            this.passwordtextbox.TabIndex = 3;
+            // 
+            // loginbutton
+            // 
+            this.loginbutton.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.loginbutton.Location = new System.Drawing.Point(277, 376);
+            this.loginbutton.Name = "loginbutton";
+            this.loginbutton.Size = new System.Drawing.Size(94, 43);
+            this.loginbutton.TabIndex = 4;
+            this.loginbutton.Text = "Login";
+            this.loginbutton.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(486, 376);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // loginpicture
+            // 
+            this.loginpicture.Image = ((System.Drawing.Image)(resources.GetObject("loginpicture.Image")));
+            this.loginpicture.Location = new System.Drawing.Point(307, 12);
+            this.loginpicture.Name = "loginpicture";
+            this.loginpicture.Size = new System.Drawing.Size(200, 200);
+            this.loginpicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.loginpicture.TabIndex = 6;
+            this.loginpicture.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // loginform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 508);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.loginpicture);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.loginbutton);
+            this.Controls.Add(this.passwordtextbox);
+            this.Controls.Add(this.usernametextbox);
+            this.Controls.Add(this.passwordlabel);
+            this.Controls.Add(this.usernamelabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "loginform";
+            this.Text = "Login Form";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.loginpicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label usernamelabel;
+        private System.Windows.Forms.Label passwordlabel;
+        private System.Windows.Forms.TextBox usernametextbox;
+        private System.Windows.Forms.TextBox passwordtextbox;
+        private System.Windows.Forms.Button loginbutton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox loginpicture;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
